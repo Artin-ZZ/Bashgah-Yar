a3261676be5c4beed3e0517283ce6daca0c29b59
Artin Zafari <90506189+Artin-ZZ@users.noreply.github.com>
2024-01-26 06:29:59 -0600

    Add files via upload

---
+++ Root/Settings/config.py
@@ -0,0 +1,7 @@
+### Data Bases ###
+ADMIN_DB_FILE = 'Update/Root/AAD/ARCdriver.zeta'
+MEMBER_DB_FILE = 'Update/Root/MMD/MRCdriver.zeta'
+SALES_DB_FILE = 'Update/Root/FFS/SLAdriver.zeta'
+
+
+### Settings ###

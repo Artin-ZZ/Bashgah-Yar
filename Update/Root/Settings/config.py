3c7ee3ff763ca861114a8dfb24ff2d400811f7ff
Artin Zafari <90506189+Artin-ZZ@users.noreply.github.com>
2024-01-26 06:28:34 -0600

    Delete Update directory

--- Update/Root/Settings/config.py
+++
@@ -1,7 +0,0 @@
-### Data Bases ###
-ADMIN_DB_FILE = 'Update/Root/AAD/ARCdriver.zeta'
-MEMBER_DB_FILE = 'Update/Root/MMD/MRCdriver.zeta'
-SALES_DB_FILE = 'Update/Root/FFS/SLAdriver.zeta'
-
-
-### Settings ###
